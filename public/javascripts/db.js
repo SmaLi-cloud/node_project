8cc6ea4f025a4f7731cbfba5f19b965bd9cf1398
userName <userEmail>
2024-12-30 09:23:38 +0800

    修改

--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -3,11 +3,11 @@ const mysql = require("mysql2");
 
 // 创建连接池
 const pool = mysql.createPool({
-  host: "localhost",
-  port: 3306,
-  user: "root", // MySQL 用户名
-  password: "123456lxz", // MySQL 密码
-  database: "tracker", // 你要连接的数据库
+  host: "sh-cynosdbmysql-grp-621v0hsm.sql.tencentcdb.com",
+  port: 21562,
+  user: "lxz", // MySQL 用户名
+  password: "123456@Lxz", // MySQL 密码
+  database: "nodejs_demo", // 你要连接的数据库
   waitForConnections: true,
   connectionLimit: 10, // 最大连接数
   queueLimit: 0,
